c854dba0415f56020589f36ba8032753f72f7fac
rhindawi <hindawi.radi@gmail.com>
2016-11-12 10:14:11 -0500

    Update PLOT_Two.R

--- PLOT_Two.R
+++ PLOT_Two.R
@@ -9,6 +9,6 @@ subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot2.png", width=480, height=480)
+png("PLOT_two.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
