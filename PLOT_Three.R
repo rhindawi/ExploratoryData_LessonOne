bc4b430d2e4fcc32273a7b0fcb1308637fdf21c3
rhindawi <hindawi.radi@gmail.com>
2016-11-12 10:10:49 -0500

    Create PLOT_Three.R

---
+++ PLOT_Three.R
@@ -0,0 +1,23 @@
+## Welcome -- Some of the header codes will be the same across the plots.
+## I changed the name of the file cause it caused on an error on my mac. No idea why.
+
+power <- read.csv("household_power_consumption_NEWNAME.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## The file is massive BTW, so I had to change a few things to get it to show.
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
+
+# THIS ONE WAS PAINFUL!!!!!!!!!!!!!!!!!!!!!111
