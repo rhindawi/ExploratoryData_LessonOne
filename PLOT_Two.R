d071d59bc5434975f27da50a7f2d9554e61356a1
rhindawi <hindawi.radi@gmail.com>
2016-11-12 10:07:37 -0500

    Update PLOT_Two.R

--- PLOT_Two.R
+++ PLOT_Two.R
@@ -2,17 +2,13 @@
 ## I changed the name of the file cause it caused on an error on my mac. No idea why.
 
 power <- read.csv("household_power_consumption_NEWNAME.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
-dataFile <- "./data/household_power_consumption_NEWNAME.txt"
-
-## The file is massive BTW, so I had to change a few things to get it to show.
-
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
+## The file is massive BTW, so I had to change a few things to get it to show.
 
-# DONE! :-)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
