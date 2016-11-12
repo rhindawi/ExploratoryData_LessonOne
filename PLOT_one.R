3d3e7bd77c70995295f5b85a99c26449d81fe965
rhindawi <hindawi.radi@gmail.com>
2016-11-12 10:14:51 -0500

    Update PLOT_one.R

--- PLOT_one.R
+++ PLOT_one.R
@@ -11,7 +11,7 @@ subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #str(subSetData)
 globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
+png("PLOT_one.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
 
